b00397c38c6017a728004b1bb7401c31defa2869
james-jeon <54837381+james-jeon@users.noreply.github.com>
2021-04-11 21:53:10 +0900

    7장 lifecycle

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { Component } from 'react';
+import { Component, useDebugValue } from 'react';
 import MyComponent from "./MyComponent";
 import Counter from './Counter';
 import Say from './Say';
@@ -8,13 +8,39 @@ import EventPractice from './EventPractice';
 import ValidationSample from './ValidationSample';
 import ScrollBox from './ScrollBox';
 import IterationSample from './IterationSample';
+import LifeCycleSample from './LifeCycleSample';
+import ErrorBoundary from './ErrorBoundary';
 
 import ClassComponent from './ClassComponent';
 import FunctionComponent from './FunctionComponent';
 
+//랜덤 색상 생성
+const getRandomColor = () => `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
 class App extends Component {
+  state = {
+    color: '#000000'
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  }
   render() {
-    const name = "리액트";
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color}/>
+        </ErrorBoundary>
+      </div>
+    );
+  }
+}
+// class App extends Component {
+//   render() {
+    // const name = "리액트";
     // return <MyComponent name={name} favoriteNumber={5}>리액트</MyComponent>;
     // return (
     //   <>
@@ -31,12 +57,13 @@ class App extends Component {
     //     <button onClick={() => this.scrollBox.scrollToBottom()}>맨 밑으로</button>
     //   </>
     // );
-    return <IterationSample/>;
+    // return <IterationSample/>;
+    // return <LifeCycleSample color={}/>
     // return <ClassComponent name={name}/>;
     // return <FunctionComponent name={ name } number={ 3223 }/>;
     // return <FunctionComponent/>;
-  }
-}
+//   }
+// }
 
 // const App = () => {
 //   return <ValidationSample/>;
---
+++ src/LifeCycleSample.js
@@ -0,0 +1,77 @@
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
+
+class LifeCycleSample extends Component {
+    state = {
+        number: 0,
+        color: null
+    };
+    myRef = null;
+
+    constructor(props) {
+        super(props);
+        console.log('constructor');
+    }
+
+    static getDerivedStateFromProps(nextProps, prevState) {
+        console.log('getDerivedStateFromProps');
+        if (nextProps.color !== prevState.color) {
+            return { color: nextProps.color }; //현재와 지금이 다를 때만 값 업데이트
+        }
+        return null;
+    }
+
+    componentDidMount() {
+        console.log('componentDidMount');
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('shouldComponentUpdate: ', nextProps, nextState);
+        return nextState.number % 10 !== 4;//숫자의 마지막 자리가 4면 리렌더링을 안한다.
+    }
+
+    componentWillUnmount() {
+        console.log("componentWillUnmount");
+    }
+
+    handleClick = () => {
+        this.setState({
+            number: this.state.number + 1
+        });
+    };
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log('getSnapshotBeforeUpdate');
+        if (prevProps.color !== this.props.color) {
+            return this.myRef.style.color;
+        }
+        return null;
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('componentDidUpdate');
+        if (snapshot) {
+            console.log('color before update ', snapshot);
+        }
+    }
+
+    render() {
+        console.log('render');
+        const style = {
+            color: this.props.color
+        };
+
+        return (
+            <div>
+                {this.props.missing.value}
+                <h1 style={style} ref={ref=>this.myRef=ref}>
+                    {this.state.number}
+                </h1>
+                <p>color: {this.state.color}</p>
+                <button onClick={this.handleClick}>더하기</button>
+            </div>
+        );
+    }
+}
+
+export default LifeCycleSample;
