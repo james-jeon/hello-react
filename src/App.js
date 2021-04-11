8abfec1861b2447c0f43733bfc6d47aef9f3cfda
james-jeon <54837381+james-jeon@users.noreply.github.com>
2021-04-11 20:58:04 +0900

    5장 ref

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import MyComponent from "./MyComponent";
 import Counter from './Counter';
 import Say from './Say';
 import EventPractice from './EventPractice'
+import ValidationSample from './ValidationSample'
+import ScrollBox from './ScrollBox';
 
 import ClassComponent from './ClassComponent';
 import FunctionComponent from './FunctionComponent';
@@ -20,11 +22,22 @@ class App extends Component {
     //     <Say/>
     //   </>
     // );
-    return <EventPractice/>;
+    // return <EventPractice/>;
+    return (
+      <>
+        <ValidationSample/>
+        <ScrollBox ref={(ref) => this.scrollBox=ref}/>
+        <button onClick={() => this.scrollBox.scrollToBottom()}>맨 밑으로</button>
+      </>
+    );
     // return <ClassComponent name={name}/>;
     // return <FunctionComponent name={ name } number={ 3223 }/>;
     // return <FunctionComponent/>;
   }
 }
+
+// const App = () => {
+//   return <ValidationSample/>;
+// }
 
 export default App
